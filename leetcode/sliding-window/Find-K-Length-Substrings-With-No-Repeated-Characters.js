e870e3fa09f5f5e0dfb5a9c37a58b31beac587fb
atorbek <atorbek@naumen.ru>
2021-05-18 23:09:36 +0300

    Find K-Length Substrings With No Repeated Characters

--- leetcode/sliding-window/Find-K-Length-Substrings-With-No-Repeated-Characters.js
+++ leetcode/sliding-window/Find-K-Length-Substrings-With-No-Repeated-Characters.js
@@ -16,5 +16,63 @@
  * Notice K can be larger than the length of S. In this case is not possible to find any substring.
  */
 function numKLenSubstrNoRepeats(str, k) {
-  return 0;
+  let f = 0;
+  let l = k;
+  let counter = 0;
+
+  let repeaterMap = new Map();
+
+  for (let i = f; i < l; i++) {
+    if (repeaterMap.has(str[i])) {
+      repeaterMap.set(str[i], repeaterMap.get(str[i]) + 1);
+    } else {
+      repeaterMap.set(str[i], 1);
+    }
+
+    if (l - i === 1) {
+      if (repeaterMap.size === k) {
+        counter++;
+      }
+
+      if (str.length !== l) {
+        i = f++;
+        l++;
+        repeaterMap = new Map();
+      }
+    }
+  }
+
+  return counter;
 }
+
+function numKLenSubstrNoRepeats1(str, k) {
+  let f = 0;
+  let l = k;
+  let counter = 0;
+
+  if (str.length < k) {
+    return counter;
+  }
+
+  while (str.length >= l) {
+    const repeaterSet = new Set([...str.substring(f, l)]);
+
+    if (repeaterSet.size === k) {
+      counter++;
+    }
+
+    f++;
+    l++;
+  }
+
+  return counter;
+}
+
+let b = new Date().getMilliseconds();
+console.log(numKLenSubstrNoRepeats('havefunonleetcode', 5)); // O(n^2)
+let e = new Date().getMilliseconds();
+console.log(e - b + 'ms');
+b = new Date().getMilliseconds();
+console.log(numKLenSubstrNoRepeats1('havefunonleetcode', 5)); // O(n)
+e = new Date().getMilliseconds();
+console.log(e - b + 'ms');
