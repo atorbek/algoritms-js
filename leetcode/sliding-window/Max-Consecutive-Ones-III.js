3074bd4296baa238cc9f34f1483ec1d40df0483a
atorbek <atorbek@naumen.ru>
2021-05-25 10:13:53 +0300

    Max Consecutive Ones III

---
+++ leetcode/sliding-window/Max-Consecutive-Ones-III.js
@@ -0,0 +1,25 @@
+/**
+ * Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
+ * Output: 6
+ * Explanation: [1,1,1,0,0,1,1,1,1,1,1]
+ * Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+ *
+ * Example 2:
+ *
+ * Input: nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], k = 3
+ * Output: 10
+ * Explanation: [0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1]
+ * Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+ */
+function longestOnes(nums, k) {}
+
+let b = new Date().getMilliseconds();
+console.log(longestOnes([1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0], 2)); // O(n)
+let e = new Date().getMilliseconds();
+console.log(e - b + 'ms');
