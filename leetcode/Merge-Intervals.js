35ac29b3172b3444171536d88cde00256f4c208e
Alexander Torbek <atorbek@naumen.ru>
2023-01-07 00:20:14 +0300

    56. Merge Intervals - https://leetcode.com/problems/merge-intervals/

--- leetcode/Merge-Intervals.js
+++ leetcode/Merge-Intervals.js
@@ -9,25 +9,32 @@ module.exports.leetcode = {
 };
 
 /**
- * Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals,
- * and return an array of the non-overlapping intervals that cover all the intervals in the input.
- *
- *
- * Example 1:
- *
- * Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
- * Output: [[1,6],[8,10],[15,18]]
- * Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
- *
- * Example 2:
- *
- * Input: intervals = [[1,4],[4,5]]
- * Output: [[1,5]]
- * Explanation: Intervals [1,4] and [4,5] are considered overlapping.
- **/
-function merge(intervals) {
-  return [];
-}
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function (intervals) {
+  intervals.sort((a, b) => a[0] - b[0]);
+
+  let res = [];
+
+  let end = intervals[0];
+
+  for (let i = 1; i < intervals.length; i++) {
+    if (intervals[i][0] <= end[1]) {
+      end = [
+        Math.min(end[0], intervals[i][0]),
+        Math.max(end[1], intervals[i][1])
+      ];
+    } else {
+      res.push(end);
+      end = intervals[i];
+    }
+  }
+
+  res.push(end);
+
+  return res;
+};
 
 intervals = [
   [1, 3],
@@ -36,7 +43,6 @@ intervals = [
   [15, 18]
 ];
 
-let b = new Date().getMilliseconds();
+console.time('time');
 console.log(merge(intervals));
-let e = new Date().getMilliseconds();
-console.log(e - b + 'ms');
+console.timeEnd('time');
