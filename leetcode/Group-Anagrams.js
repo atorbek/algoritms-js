41c9d2cc261b5c3d4dfb09bed7453ba0a6c0a138
Alexander Torbek <atorbek@naumen.ru>
2023-02-28 00:06:55 +0300

    49. Group Anagrams, refactored O(n*klogk) -> O(n*k) - https://leetcode.com/problems/group-anagrams/

--- leetcode/Group-Anagrams.js
+++ leetcode/Group-Anagrams.js
@@ -13,6 +13,47 @@ module.exports.leetcode = {
  * @return {string[][]}
  */
 var groupAnagrams = function (strs) {
+  if (strs.length === 0) {
+    return [];
+  }
+
+  let dict = {};
+
+  const count = [];
+
+  for (let str of strs) {
+    // n
+
+    // array will be filled by 0
+    for (let i = 0; i < 26; i++) {
+      count[i] = 0;
+    }
+
+    // create key by str
+    for (let i = 0; i < str.length; i++) {
+      let charCode = str[i].charCodeAt(0) - 'a'.charCodeAt(0);
+      count[charCode] += 1;
+    }
+
+    let charCodesAsString = count.join('#');
+
+    // ^3k
+
+    if (Array.isArray(dict[charCodesAsString])) {
+      dict[charCodesAsString].push(str);
+    } else {
+      dict[charCodesAsString] = [str];
+    }
+  } // O(n*3k) => O(n*k)
+
+  return [...Object.values(dict)];
+};
+
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams2 = function (strs) {
   let dict = {};
 
   for (let i = 0; i < strs.length; i++) {
@@ -27,7 +68,8 @@ var groupAnagrams = function (strs) {
 
   return [...Object.values(dict)]; // n
 };
-https: console.time('time');
+
+console.time('time');
 console.log(groupAnagrams(['eat', 'tea', 'tan', 'ate', 'nat', 'bat']));
 console.log(groupAnagrams(['']));
 console.log(groupAnagrams(['a']));
