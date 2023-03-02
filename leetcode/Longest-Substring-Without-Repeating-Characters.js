7fced34d8e30c5e93b1ca6a023852919b7a71bd3
Alexander Torbek <atorbek@naumen.ru>
2023-03-03 00:43:06 +0300

    3. Longest Substring Without Repeating Characters - https://leetcode.com/problems/longest-substring-without-repeating-characters/

--- leetcode/Longest-Substring-Without-Repeating-Characters.js
+++ leetcode/Longest-Substring-Without-Repeating-Characters.js
@@ -9,61 +9,25 @@ module.exports.leetcode = {
   premium: false
 };
 
-/**
- * Description:
- * Given a string s, find the length of the longest substring without repeating characters.
- *
- * Example 1:
- *
- * Input: s = "abcabcbb"
- * Output: 3
- * Explanation: The answer is "abc", with the length of 3.
- *
- * Example 2:
- *
- * Input: s = "bbbbb"
- * Output: 1
- * Explanation: The answer is "b", with the length of 1.
- *
- * Example 3:
- *
- * Input: s = "pwwkew"
- * Output: 3
- * Explanation: The answer is "wke", with the length of 3.
- * Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
- *
- * Example 4:
- *
- * Input: s = ""
- * Output: 0
- */
-function lengthOfLongestSubstring(str) {
-  let f = 0;
-  let l = f;
-  let count = 0;
+function lengthOfLongestSubstring(s) {
+  let ans = 0;
+  const map = {};
 
-  while (str.length >= l) {
-    const subStr = new Set([...str.substring(f, l)]);
-
-    if (subStr.size < l - f) {
-      if (count < subStr.size) {
-        count = subStr.size;
-      }
-      f++;
-      l++;
-    } else {
-      if (count < subStr.size) {
-        count = subStr.size;
-      }
-
-      l++;
+  let i = 0;
+  for (let j = 0; j < s.length; j++) {
+    if (map[s.charCodeAt(j)]) {
+      i = Math.max(map[s.charCodeAt(j)], i);
     }
+
+    ans = Math.max(ans, j - i + 1);
+    map[s.charCodeAt(j)] = j + 1;
   }
 
-  return count;
+  return ans;
 }
 
-let b = new Date().getMilliseconds();
-console.log(lengthOfLongestSubstring('a')); // O(n)
-let e = new Date().getMilliseconds();
-console.log(e - b + 'ms');
+console.time('time');
+console.log(lengthOfLongestSubstring('abcabcbb'));
+console.log(lengthOfLongestSubstring('bbbbb'));
+console.log(lengthOfLongestSubstring('pwwkew'));
+console.timeEnd('time');
