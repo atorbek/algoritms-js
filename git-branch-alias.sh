dcce6604dc39c755b6e745c8512bc0de8c6dbcd2
Alexander Torbek <atorbek@naumen.ru>
2023-03-09 00:41:52 +0300

    Added alias for easy branch leetcode creation

---
+++ git-branch-alias.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+##########################################################################
+
+# Before using
+#
+# 1. Open .git/config and add 2 lines:
+#
+# [alias]
+#	leetbranch = "!$HOME/projects/algoritms-js/git-branch-alias.sh \"$1\""
+#
+# 2. Save file .git/config
+# 3. Enter Command in terminal: git leetbranch
+#
+# This command replaces spaces to en dash and upper to lower caces.
+
+###########################################################################
+
+str=$1
+branch=$(echo "${str// /-}" | tr '[:upper:]' '[:lower:]')
+git branch  -D $branch
+git checkout -b $branch
